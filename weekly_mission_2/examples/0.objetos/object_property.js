7435c2cb38632c0dc34c561034792e1c3705bc7a
MikoVelascoRamirez <launchx08269@innovaccion.mx>
2022-04-13 17:50:38 -0500

    feat: creating diff_properties excercise

---
+++ weekly_mission_2/examples/0.objetos/object_property.js
@@ -0,0 +1,18 @@
+//Objeto con diferentes propiedades
+const myObject3 = {
+  name: "Dory",
+  age: 5,
+  nicknames: [
+    "La gorda", "Dorycita"
+  ],
+  address: {
+    zip_code: "10000",
+    street: "P. Shelman, Calle Wallaby 2",
+    city: "Sydney"
+  }
+};
+
+console.log("Ejemplo 3: Objeto con diferentes propiedades");
+console.log(myObject3);
+console.log(myObject3.name);
+console.log(myObject3["address"]);
