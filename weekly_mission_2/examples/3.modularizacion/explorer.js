3ff73e69a7145905e0ca8d2ffba06a7f62dc5664
MikoVelascoRamirez <launchx08269@innovaccion.mx>
2022-04-15 17:04:27 -0500

    feat: explorer.js file added

---
+++ weekly_mission_2/examples/3.modularizacion/explorer.js
@@ -0,0 +1,9 @@
+export default class Explorer {
+    constructor(name, username, mission){ 
+        this.name = name;
+        this.username = username;
+        this.mission = mission;
+    }
+
+    getNameAndUsername(){ return `Explorer: ${this.name} Username: ${this.username}`}
+}
