76abb9336e45acb5decb9a0ca6aee3b55a31c5fe
MikoVelascoRamirez <launchx08269@innovaccion.mx>
2022-04-14 22:25:39 -0500

    static_methods excercise

---
+++ weekly_mission_2/examples/2.clases/8.static_methods.js
@@ -0,0 +1,15 @@
+// Ejemplo 8: Métodos static nos ayudan a escribir métodos en una clase que podemos usar sin necesidad de instanciar algún objeto
+
+class Toolbox{
+    static getMostUsefulMethods(){ return ["Command Line", "git", "Text Editor"] }
+}
+
+console.log("Ejemplo 8: Métodos static")
+// Puedo llamar el método static directamente con el nombre de la clase
+
+const toolbox = new Toolbox();
+
+//al definir un método estático, no debemos instanciar un objeto como medio para utilizarlo.
+//console.log(toolbox.getMostUsefulMethods()) //Esto NO ESTÁ PERMITIDO
+
+console.log(Toolbox.getMostUsefulMethods()) //Esto SI ESTÁ PERMITIDO
