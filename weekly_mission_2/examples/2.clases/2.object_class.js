c06a3f08652fe590a2ad27f8f2627b9e1994533e
MikoVelascoRamirez <launchx08269@innovaccion.mx>
2022-04-14 19:35:45 -0500

    feat: object_class excercise

---
+++ weekly_mission_2/examples/2.clases/2.object_class.js
@@ -0,0 +1,8 @@
+// Ejemplo 2: Crear un objeto a partir de una clase
+
+class Pet{    
+}
+
+const myPet1 = new Pet();
+console.log("Ejemplo 2: Crear un objeto a partir de una clase")
+console.log(myPet1) // un objeto de la clase Pet
