1bd18464f17ba4f54a56a63cd28264768be7af14
MikoVelascoRamirez <launchx08269@innovaccion.mx>
2022-04-19 17:54:02 -0500

    feat: writing biography object definition

---
+++ weekly_mission_2/excercises/excercise-facebook/biography.js
@@ -0,0 +1,68 @@
+const Biography = {
+  idProfile: "sdasmdk1301229",
+  profileName: "Juanito Ṕérez",
+  info: {
+    workPlace: [
+        {
+            nameCompany: "Bimbo",
+            role: "Software Analyst",
+            city: "Pachuca de Soto",
+            description : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Amet volutpat consequat mauris nunc congue nisi.",
+            startFrom: "Junio 2020",
+            currentWork: true,
+            privacity: "public"
+        }
+    ],
+    schooling: [
+        {
+            university: {
+                name: "Universidad de las Américas",
+                startFrom: "Agosto 2012",
+                finishTo: "Junio 2016",
+                wasFinished: true,
+                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Amet volutpat consequat mauris nunc congue nisi.",
+                degree: "Licenciatura en Ingeniería en Software",
+                privacity: "public"
+            },
+            high_school: {
+                name: "Esc. Sec. Fernando Urzúa",
+                startFrom: "Agosto 2006",
+                finishTo: "Junio 2016",
+                wasFinished: true,
+                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Amet volutpat consequat mauris nunc congue nisi.",
+                degree: "Certificado de secundaria",
+                privacity: "private"
+            }
+        }
+    ],
+    livePlaces: [
+        {
+            city: "Guadalajara",
+            currentCity: true,
+            privacity: "public"
+        },
+        { 
+            city: "Ciudad de México",
+            currentCity: false,
+            privacity: "private"
+        }
+    ],
+    basic: {
+        bornPlace: "Acapulco",
+        phone: "1278312111",
+        email: "juanito98@gmail.com",
+        gender: "masculino",
+        bornDate: "14 de Febrero",
+        year: 1992,
+        status: "soltero",
+    },
+    about: "Vitae suscipit tellus mauris a diam maecenas sed enim ut. Placerat vestibulum lectus mauris ultrices eros in cursus turpis massa. Nunc scelerisque viverra mauris in aliquam sem. Nec nam aliquam sem et.",
+    nicknames: ["Juancho", "El milaneso"],
+    specialEvents: [
+        {
+            category: "work",
+            description: "Empecé a trabajar en Bimbo en la hermosa ciudad de Guadalajara, hogar del tequila"
+        }
+    ]
+  }
+};
