a948c1211819fb7bad5b8394146749d4f65b3217
MikoVelascoRamirez <launchx08269@innovaccion.mx>
2022-04-20 15:33:43 -0500

    feat: req 1. Printing each explorer name of the list

---
+++ weekly_mission_2/excercises/Ejercicio_2/requeriments.js
@@ -0,0 +1,7 @@
+const explorers = require('./explorers');
+
+// Lista de requerimientos a realizar sobre archivo de explorers.js
+
+// 1. Imprime el nombre (propiedad name) de cada explorer en la lista, usa FOR EACH
+
+explorers.forEach( explorer => console.log(explorer.name))
