ea466d316ccf6e1d5be418151339a60b0288bd4b
MikoVelascoRamirez <launchx08269@innovaccion.mx>
2022-04-20 15:35:01 -0500

    feat: req 2. Printing each explorer stack of the list

--- weekly_mission_2/excercises/Ejercicio_2/requeriments.js
+++ weekly_mission_2/excercises/Ejercicio_2/requeriments.js
@@ -5,3 +5,7 @@ const explorers = require('./explorers');
 // 1. Imprime el nombre (propiedad name) de cada explorer en la lista, usa FOR EACH
 
 explorers.forEach( explorer => console.log(explorer.name))
+
+// 2. Imprime el stack de cada explorer, usa FOR EACH
+
+explorers.forEach( explorer => console.log(explorer.stack))
