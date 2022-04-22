02382082abd80b59e25dc049dd4fb16f55efebed
MikoVelascoRamirez <launchx08269@innovaccion.mx>
2022-04-22 15:13:42 -0500

    feat: writing Hashtag's constructor

---
+++ weekly_mission_2/excercises/Ejercicio_3/excercise-twitter-classes/Hashtag.js
@@ -0,0 +1,10 @@
+class Hashtag{
+    constructor(name, category, relatedTrends, trending){
+        this.name = name;
+        this.category = category;
+        this.relatedTrends = relatedTrends;
+        this.outstandings = [];
+        this.most_recents = [];
+        this.trending = trending;
+    }
+}
