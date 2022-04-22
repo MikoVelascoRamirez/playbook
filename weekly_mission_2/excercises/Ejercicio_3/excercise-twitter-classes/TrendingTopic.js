8553242ef7437432b1f5d8b085d4af24564ae18e
MikoVelascoRamirez <launchx08269@innovaccion.mx>
2022-04-22 16:32:25 -0500

    feat: adding new methods

--- weekly_mission_2/excercises/Ejercicio_3/excercise-twitter-classes/TrendingTopic.js
+++ weekly_mission_2/excercises/Ejercicio_3/excercise-twitter-classes/TrendingTopic.js
@@ -37,6 +37,18 @@ class TrendingTopic{
         ${this.trending ? 'Tendencias' : ''}`;
     }
 
+    get getPeople(){
+        return this.people;
+    }
+
+    get getPhotos(){
+        return this.photos;
+    }
+
+    get getVideos(){
+        return this.videos;
+    }
+
     //Setters
     set addRecentTweet(tweet){
         this.most_recents = [...this.most_recents, tweet];
@@ -63,3 +75,122 @@ class TrendingTopic{
 const tt = new TrendingTopic("Riqui", "Deportes", ["Piqué", "FC Barcelona"], true);
 
 //Implementation
+tt.addRecentTweet = {
+    idTweet: 123434354,
+    datePublished: "8/02/2021",
+    hourPublished: "20:21",
+    tweetContent: "Visca el Barca",
+    media: [],
+    comments: [
+      {
+        idTweet: 10291291,
+        user: "@ZonaBlaugrana",
+        datePublished: "8/02/2021",
+        hourPublished: "20:34",
+        sourceLabel: "Twitter Web App",
+        retweets: 0,
+        respondTo: 123434354,
+        comments: 0
+      }
+    ],
+    sourceLabel: "Twitter for iPhone"
+}
+
+tt.addOutstandingTweet = {
+    idTweet: 123434354,
+    datePublished: "8/02/2021",
+    hourPublished: "20:21",
+    tweetContent: "Visca el Barca",
+    media: [],
+    comments: [
+      {
+        idTweet: 10291291,
+        user: "@ZonaBlaugrana",
+        datePublished: "8/02/2021",
+        hourPublished: "20:34",
+        sourceLabel: "Twitter Web App",
+        retweets: 0,
+        respondTo: 123434354,
+        comments: 0
+      }
+    ],
+    sourceLabel: "Twitter for iPhone"
+} 
+
+tt.addRecentTweet = {
+    idTweet: 123434354,
+    datePublished: "19/03/2022",
+    hourPublished: "10:00",
+    tweetContent: "hola a todos",
+    media: [],
+    comments: [
+      {
+        idTweet: 10291291,
+        user: "@LaCocinaDeKaty",
+        datePublished: "19/03/2022",
+        hourPublished: "13:56",
+        sourceLabel: "Twitter for iPhone",
+        retweets: 1,
+        respondTo: 123434354,
+        comments: 0
+      }
+    ],
+    sourceLabel: "Twitter for iPhone"
+}
+
+tt.addOutstandingTweet = {
+    idTweet: 123434354,
+    datePublished: "19/03/2022",
+    hourPublished: "10:00",
+    tweetContent: "hola a todos",
+    media: [],
+    comments: [
+      {
+        idTweet: 10291291,
+        user: "@LaCocinaDeKaty",
+        datePublished: "19/03/2022",
+        hourPublished: "13:56",
+        sourceLabel: "Twitter for iPhone",
+        retweets: 1,
+        respondTo: 123434354,
+        comments: 0
+      }
+    ],
+    sourceLabel: "Twitter for iPhone"
+}
+
+tt.addPeople = {
+    userName: "Riqui Puig",
+    profileName: "@RiquiPuig",
+    followed: true
+}
+
+tt.addPeople = {
+    userName: "Riqui Gell",
+    profileName: "@riquigell",
+    followed: false
+}
+
+tt.addPeople = {
+    userName: "Riqui Hispanidad",
+    profileName: "@libertadrichi",
+    followed: false
+}
+
+tt.addPhoto = {
+    media: [
+      "https://pbs.twimg.com/media/FQqiyhVWYBAhIfT?format=jpg&name=large", "https://pbs.twimg.com/media/FQqiywjWQAYv-92?format=jpg&name=large"
+    ],
+    tweetId: 10291921
+}
+
+tt.addVideo = {
+    media: ["https://twitter.com/i/status/1515997418017497097"],
+    tweetId: 12010212
+}
+
+console.log(`${tt.getGeneralInfo}`);
+console.log("Recent Tweets: ")
+tt.getMostRecentTweets
+console.log("Destacados: ")
+tt.getOutstandings
