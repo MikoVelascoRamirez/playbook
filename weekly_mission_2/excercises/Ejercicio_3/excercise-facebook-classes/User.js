71aaeb4fd7d93182131c0388daca2fa83e111e45
MikoVelascoRamirez <launchx08269@innovaccion.mx>
2022-04-22 01:22:04 -0500

    refactor: add photos attribute

--- weekly_mission_2/excercises/Ejercicio_3/excercise-facebook-classes/User.js
+++ weekly_mission_2/excercises/Ejercicio_3/excercise-facebook-classes/User.js
@@ -7,6 +7,7 @@ class User{
         this.pictureProfile = pictureProfile;
         this.portraitPicture = portraitPicture;
         this.posts = [];
+        this.photos = [];
     }
 
     //Getters
