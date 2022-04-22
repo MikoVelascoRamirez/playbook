c1bb0cfff1abb22c8c4e25e3a9ecaee6a0b49de5
MikoVelascoRamirez <launchx08269@innovaccion.mx>
2022-04-22 16:17:37 -0500

    feat: writing Trending Topic getters and setters

--- weekly_mission_2/excercises/Ejercicio_3/excercise-twitter-classes/TrendingTopic.js
+++ weekly_mission_2/excercises/Ejercicio_3/excercise-twitter-classes/TrendingTopic.js
@@ -10,4 +10,55 @@ class TrendingTopic{
         this.videos = [];
         this.trending = trending;
     }
+
+    //Getters
+    get getNumberOfTweets(){
+        return this.most_recents.length;
+    }
+
+    get getNumberOfUsers(){
+        return this.people.length
+    }
+
+    get getOutstandings(){
+        return this.outstandings.filter( outstanding => outstanding.tweetContent.includes("hola"))
+        .forEach( outstanding => console.log(outstanding));
+    }
+
+    get getMostRecentTweets(){
+        return this.most_recents.forEach( most_recent => console.log(most_recent));
+    }
+
+    get getGeneralInfo(){
+        return `Hashtag: ${this.name}
+        Category: ${this.category}
+        Tweets: ${this.getNumberOfTweets}        
+        Related Trends: ${this.relatedTrends}
+        ${this.trending ? 'Tendencias' : ''}`;
+    }
+
+    //Setters
+    set addRecentTweet(tweet){
+        this.most_recents = [...this.most_recents, tweet];
+    }
+
+    set addOutstandingTweet(tweet){
+        this.outstandings = [...this.outstandings, tweet];
+    }
+
+    set addPeople(person){
+        this.people = [...this.people, person];
+    }
+
+    set addPhoto(photo){
+        this.photo = [...this.photo, photo];
+    }
+
+    set addVideo(video){
+        this.video = [...this.video, video];
+    }
 }
+
+//Instance
+
+//Implementation
