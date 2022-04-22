36eb76d460d6b3a91df701a4f23473fb208c88a7
MikoVelascoRamirez <launchx08269@innovaccion.mx>
2022-04-22 16:05:15 -0500

    feat: adding treanding topic's constructor

---
+++ weekly_mission_2/excercises/Ejercicio_3/excercise-twitter-classes/TrendingTopic.js
@@ -0,0 +1,13 @@
+class TrendingTopic{
+    constructor(name, category, relatedTrends, trending){
+        this.name = name;
+        this.category = category;
+        this.relatedTrends = relatedTrends;
+        this.outstandings = [];
+        this.most_recents = [];
+        this.people = [];
+        this.photos = [];
+        this.videos = [];
+        this.trending = trending;
+    }
+}
