a718f05d0a0169ecf01032fbda7cf7e4a09f818a
MikoVelascoRamirez <launchx08269@innovaccion.mx>
2022-04-22 15:22:09 -0500

    feat: writing Hashtag's getters and setters

--- weekly_mission_2/excercises/Ejercicio_3/excercise-twitter-classes/Hashtag.js
+++ weekly_mission_2/excercises/Ejercicio_3/excercise-twitter-classes/Hashtag.js
@@ -7,4 +7,44 @@ class Hashtag{
         this.most_recents = [];
         this.trending = trending;
     }
+
+    //Getters
+    get getNumberOfTweets(){
+        return this.most_recents.length;
+    }
+
+    get getNumberOfUsers(){
+        return this.people.length
+    }
+
+    get getGeneralInfo(){
+        return `Hashtag: ${this.name}
+        Category: ${this.category}
+        Tweets: ${this.getNumberOfTweets}
+        Related Trends: ${this.relatedTrends}
+        ${this.trending ? 'Tendencias' : ''}`;
+    }
+
+    get getOutstandings(){
+        return this.outstandings.filter( outstanding => outstanding.tweetContent.includes("hola"));
+    }
+
+    get getMostRecentTweets(){
+        return this.most_recents.map( most_recent => most_recent);
+    }
+
+    //Setters
+
+    set addRecentTweet(tweet){
+        this.most_recents = [...this.most_recents, tweet];
+    }
+
+    set addOutstandingTweet(tweet){
+        this.outstandings = [...this.outstandings, tweet];
+    }
 }
+
+//Instances
+
+
+//Implementation
