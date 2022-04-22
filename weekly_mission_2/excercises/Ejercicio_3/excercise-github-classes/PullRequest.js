2e34c1950a67a6535afccac8c904a35ba167fd04
MikoVelascoRamirez <launchx08269@innovaccion.mx>
2022-04-22 14:11:21 -0500

    feat: writing PullRequest getters and setters

--- weekly_mission_2/excercises/Ejercicio_3/excercise-github-classes/PullRequest.js
+++ weekly_mission_2/excercises/Ejercicio_3/excercise-github-classes/PullRequest.js
@@ -7,4 +7,18 @@ class PullRequest{
         this.author = author;
         this.repositoryNameAssociated = repositoryNameAssociated;
     }
+
+    //Getters
+    get getStatus(){
+        return `The PR with the name "${this.title}" is ${this.status}`
+    }
+
+    get getTitleAndAuthor(){
+        return `The Pull Request with the name "${this.title}" was created by ${this.author}`
+    }
+
+    //Setters
+    set setStatus(status){
+        this.status = status;
+    }
 }
