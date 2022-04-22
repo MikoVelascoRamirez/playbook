8fd7f92fe4ec3834ae26d0ea67df6105d0545e0d
MikoVelascoRamirez <launchx08269@innovaccion.mx>
2022-04-22 14:54:20 -0500

    feat: writing Repo constructor

---
+++ weekly_mission_2/excercises/Ejercicio_3/excercise-github-classes/Repo.js
@@ -0,0 +1,12 @@
+class Repo{
+    constructor(name, author, language, numberOfCommits, stars, forks, issues_open, issues_close){
+        this.name = name;
+        this.author = author;
+        this.language = language;
+        this.numberOfCommits = numberOfCommits;
+        this.stars = stars;
+        this.forks = forks;
+        this.issues_open = issues_open;
+        this.issues_close = issues_close;
+    }
+}
