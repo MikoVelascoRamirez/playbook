6276f401058a4db7365e515b31e2f52ec2a0e08d
MikoVelascoRamirez <launchx08269@innovaccion.mx>
2022-04-22 19:19:24 -0500

    feat: writing User's getters & setters

--- weekly_mission_2/excercises/Ejercicio_3/excercise-twitter-classes/User.js
+++ weekly_mission_2/excercises/Ejercicio_3/excercise-twitter-classes/User.js
@@ -10,4 +10,48 @@ class User{
         this.following = following;
         this.tweets = [];
     }
+
+    //Getters
+    get getNumberOfFollowers(){
+        return this.followers;
+    }
+
+    get getNumberOfFollows(){
+        return this.following;
+    }
+
+    get getNumberOfTweets(){
+        return this.tweets.length;
+    }
+
+    get getBasicInfo() {
+        return `Profile: ${this.profileName}
+            ${this.userName}
+            ${this.description}
+            Se unió en ${this.registeredFrom}`;
+    }
+
+    get renderTweets() {
+        return this.tweets
+    }
+
+    //Setters
+    set tweet(tweet){
+        this.tweets = [...this.tweets, tweet];
+    }
+
+    //Methods
+
+    addNewFollower(){
+        this.followers += 1;
+    }
+
+    newFollow(){
+        this.following += 1;
+    }
 }
+
+//Instance
+
+
+//Implementation
