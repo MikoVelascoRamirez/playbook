eeaa82146c84f8363ecbdd0e3d5e2a36ff2c9a75
MikoVelascoRamirez <launchx08269@innovaccion.mx>
2022-04-22 21:38:33 -0500

    feat: writing Profile's getters-setters

--- weekly_mission_2/excercises/Ejercicio_3/excercise-uber-classes/Profile.js
+++ weekly_mission_2/excercises/Ejercicio_3/excercise-uber-classes/Profile.js
@@ -16,4 +16,48 @@ class Profile{
         this.thankyouNotes = [];
         this.driverAchivements = [];
     }
+
+    //Getters
+
+    get getRiderCompliments(){
+        return this.riderCompliments.map( compliment => compliment)
+    }
+
+    get getThankYouNotes(){
+        return this.thankyouNotes;
+    }
+
+    get getdriverAchievements(){
+        return this.driverAchievments;
+    }
+
+    get getProfileInfo(){
+        return `
+        Name: ${this.userName}
+        ${this.car} - ${this.license}
+        ${this.trips} trips - ${this.rating} ⭐ rating - ${this.years} years
+        Speaks ${this.languages}
+        From ${this.cityBorn}, ${this.countryBorn}
+        Fun fact: ${this.funFact}
+        Most memorable trip: ${this.mostMemorableTrip}
+        `
+    }
+
+    //Setters
+
+    set addCompliment(compliment){
+        this.riderCompliments = [...this.riderCompliments, compliment];
+    }
+
+    set addThankfulNote(note){
+        this.thankyouNotes = [...this.thankyouNotes, note];
+    }
+
+    set addAchievement(achievement){
+        this.driverAchivements = [...this.addAchievement, achievement];
+    }
 }
+
+//Instance
+
+//Implementation
