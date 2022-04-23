51d1147878ebb0648b9ad8846ed66f8a7fc5d50d
MikoVelascoRamirez <launchx08269@innovaccion.mx>
2022-04-22 19:26:16 -0500

    feat: implementing User's methods

--- weekly_mission_2/excercises/Ejercicio_3/excercise-twitter-classes/User.js
+++ weekly_mission_2/excercises/Ejercicio_3/excercise-twitter-classes/User.js
@@ -52,6 +52,42 @@ class User{
 }
 
 //Instance
-
+const apostol_cule = new User("Apóstol Culé", "@CruyffistaMX", "https://pbs.twimg.com/profile_images/1358915800883732481/Iw61tRwk_400x400.jpg", "https://pbs.twimg.com/profile_banners/1358912820998508546/1612825750/1500x500", "Blaugrana desde 2004 | Mexicano-Tijuanense | Seguidor y predicador del mejor futbol jamás visto | ¿mi pastor?, Cruyff | ¿vamos al Camp Nou? cuando quieras. 🇲🇽", 300, 66);
 
 //Implementation
+
+// --> Agregando tweets
+apostol_cule.tweet = {
+    idTweet: 123434354,
+    datePublished: "8/02/2021",
+    hourPublished: "20:21",
+    tweetContent: "Hola, le voy al FC Barcelona",
+    media: ["https://youtu.be/bC7-Yqh-svU"],
+    comments: [
+      {
+        idTweet: 10291291,
+        user: "@ZonaBlaugrana",
+        datePublished: "8/02/2021",
+        hourPublished: "20:34",
+        sourceLabel: "Twitter Web App",
+        retweets: 0,
+        respondTo: 123434354,
+        comments: 0
+      }
+    ],
+    analytics: {
+      likes: 5,
+      retweets: 1,
+      comments: 1,
+      impressions: 110,
+      interactions: 5,
+      extensionsDetails: 5,
+      viewProfiles: 0,
+      newFollowers: 0
+    },
+    sourceLabel: "Twitter for Android"
+}
+
+console.log(`${apostol_cule.getBasicInfo}`);
+console.log(`${apostol_cule.getNumberOfFollows} siguiendo ${apostol_cule.getNumberOfFollowers} seguidores`);
+console.log(apostol_cule.renderTweets);
