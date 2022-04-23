4d197625ffb465b43c12e6548810b882c9822441
MikoVelascoRamirez <launchx08269@innovaccion.mx>
2022-04-22 19:35:50 -0500

    feat: writing Profile's constructor

---
+++ weekly_mission_2/excercises/Ejercicio_3/excercise-uber-classes/Profile.js
@@ -0,0 +1,19 @@
+class Profile{
+    constructor(userName, car, photo, license, trips, rating, years, languages, cityBorn, countryBorn, funFact, mostMemorableTrip){
+        this.userName = userName;
+        this.car = car;
+        this.photo = photo;
+        this.license = license;
+        this.trips = trips;
+        this.rating = rating;
+        this.years = years;
+        this.languages = languages;
+        this.cityBorn = cityBorn;
+        this.countryBorn = countryBorn;
+        this.funFact = funFact;
+        this.mostMemorableTrip = mostMemorableTrip;
+        this.riderCompliments = [];
+        this.thankyouNotes = [];
+        this.driverAchivements = [];
+    }
+}
