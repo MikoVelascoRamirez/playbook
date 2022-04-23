6ae0040d671b8833e860b53e16ee31ed686901ee
MikoVelascoRamirez <launchx08269@innovaccion.mx>
2022-04-23 01:02:59 -0500

    feat: instance Travel's object

--- weekly_mission_2/excercises/Ejercicio_3/excercise-uber-classes/Travel.js
+++ weekly_mission_2/excercises/Ejercicio_3/excercise-uber-classes/Travel.js
@@ -36,5 +36,8 @@ class Travel{
 }
 
 // Instance
+const sofiaTravel = new Travel("32llle12p3e", "María Sofía", "Huge Jackman", "Calle Pajaritos 09-XYZ", "Zona Arqueólogica de Huapalcalco", "15:30", "16:00", "cash", 45.00, ["Siga recto por la calle pajaritos hasta calle champiñón, gire a la izquierda",
+"siga derecho 200 m hasta calle margaritas, doble a la derecha", 
+"Ha llegado a su destino: Zona Arqueológica Huapalcalco"]);
 
 //Implementation
